1543bb3b3b5b40c2b86feb7e0208041f50f7b8a6
Vlad <68771766+Spiker1992@users.noreply.github.com>
2020-11-07 16:27:51 +0000

    Added test for the set password endpoint

--- tests/Feature/test-set-password.php
+++ tests/Feature/test-set-password.php
@@ -2,14 +2,31 @@
 
 namespace Tests\Feature;
 
-use Tests\EndpointTestCase; 
+use Tests\EndpointTestCase;
+use Tests\Factory\UserFactory;
 
 class WP_Test_Set_Password extends EndpointTestCase     
 { 
     protected string $endPoint = '/bdpwr/v1/set-password'; 
 
+    /**
+     * @group current
+     */
     public function test_set_password()
     {
-        $this->markTestSkipped( 'To do.' );
+        $code = 'my_code';
+        $user = (new UserFactory)
+            ->withCode($code)
+            ->make();
+
+        $payload = array( 
+            'email' => $user['email'],
+            'code' => $code,
+            'password' => 'my_password',
+        );
+    
+        $this->call( 'POST' , $payload )
+            ->assertStatus( 200 )
+            ->assertPartialResponse( array( 'message' => 'Password reset successfully.' ) );
     }
 }
